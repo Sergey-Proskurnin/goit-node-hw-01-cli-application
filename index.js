f0266f0ba5073e881e85edf4008a17471eb189b1
Sergey-Proskurnin <proskurnin@ua.fm>
2021-08-12 00:52:32 +0300

    Add second and third steps

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('Hello i am index.js');
+const contacts = require('./contacts.js')
+
+// contacts.listContacts();
+// contacts.getContactById(10);
+// contacts.removeContact(3);
+// contacts.addContact('Jone Smitch', 'jonesmitch@ps.ua', '(045) 534-4318')
+
+
